4600d2a626dad6e0269d5a3fe28d0de685edbbea
Ruan Lopes Gomes <ruanlopes21@gmail.com>
2022-08-13 11:21:24 -0300

    CreatPost component created

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,25 @@
-import Navbar from "../src/components/layout/Navbar"
+import styled from 'styled-components'
+
+import Navbar from '../src/components/layout/Navbar'
+import Container from '../src/components/layout/Container'
+import CreatPost from '../src/components/cards/CreatPost'
+
+const Content = styled.div`
+  margin: 50px 0;
+`
 
 function HomePage () {
   
   return (
     <>
       <Navbar />
-      <div>
-        SejaDev
-      </div>
+      <Content>
+        <Container>
+          <CreatPost>
+
+          </CreatPost>
+        </Container>
+      </Content>
     </>
   )
 }
