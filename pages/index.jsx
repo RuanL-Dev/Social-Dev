56f07db7b4445e2043fe02edb3c9c02bd3955ad5
Ruan Lopes Gomes <ruanlopes21@gmail.com>
2022-08-15 20:32:20 -0300

    Post component created

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,9 +3,32 @@ import styled from 'styled-components'
 import Navbar from '../src/components/layout/Navbar'
 import Container from '../src/components/layout/Container'
 import CreatPost from '../src/components/cards/CreatPost'
+import H3 from '../src/components/typography/H3'
+import Post from '../src/components/cards/Post'
 
 const Content = styled.div`
   margin: 50px 0;
+`
+
+const LastPostText = styled(H3)`
+  padding: 40px 0;
+`
+
+const RefreshPosts = styled.span`
+  font-weight: bold;
+  color: ${props => props.theme.primary};
+  cursor: pointer;
+`
+
+const RefreshPostsContainer = styled.div`
+  text-align: center;
+`
+
+const PostContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  margin-top: 20px;
 `
 
 function HomePage () {
@@ -15,9 +38,16 @@ function HomePage () {
       <Navbar />
       <Content>
         <Container>
-          <CreatPost>
-
-          </CreatPost>
+          <CreatPost />
+            <LastPostText>Últimas postagens:</LastPostText>
+            <RefreshPostsContainer>
+              <RefreshPosts>Carregar novas postagens</RefreshPosts>
+            </RefreshPostsContainer>
+            <PostContainer>
+              <Post />
+              <Post />
+              <Post />
+            </PostContainer>
         </Container>
       </Content>
     </>
