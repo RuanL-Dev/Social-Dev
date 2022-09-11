6eb1c45e1a4ee4f4856b390156db2d431908e572
Ruan Lopes Gomes <ruanlopes21@gmail.com>
2022-09-11 14:22:13 -0300

    Post creation link to backend

--- src/components/cards/CreatPost.jsx
+++ src/components/cards/CreatPost.jsx
@@ -1,7 +1,13 @@
 import styled from 'styled-components'
+import { useForm } from 'react-hook-form'
+import { joiResolver } from '@hookform/resolvers/joi'
+import axios from 'axios'
+import { useSWRConfig } from 'swr'
+
+import { createPostSchema } from '../../../modules/post/post.schema'
 
 import H4 from '../typography/H4'
-import Textarea from '../inputs/Textarea'
+import ControlledTextArea from '../inputs/ControlledTextarea'
 import Button from '../inputs/Button'
 
 
@@ -39,16 +45,38 @@ const BottomText = styled.p`
 
 
 function CreatPost ({ username }) {
+  const { mutate } = useSWRConfig()
+  const { control, handleSubmit, formState: { isValid }, reset } = useForm({
+    resolver: joiResolver(createPostSchema),
+    mode: 'all'
+  })
+
+  const onSubmit = async (data) => {
+    const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/api/post`, data)
+    if (response.status === 201) {
+      reset()
+      mutate(`${process.env.NEXT_PUBLIC_API_URL}/api/post`)
+    }
+  }
+
   return(
     <PostContainer>
       <H4><Title>No que você está pensando, @{username}?</Title></H4>
-      <TextContainer>
-        <Textarea placeholder="Digite sua mensagem" rows="4" />
-      </TextContainer>
-      <BottomContainer>
-        <BottomText>A sua mensagem será pública.</BottomText>
-        <Button>Enviar mensagem</Button>
-      </BottomContainer>
+      <form onSubmit={handleSubmit(onSubmit)} >
+        <TextContainer>
+          <ControlledTextArea 
+            placeholder="Digite sua mensagem" 
+            rows="4" 
+            control={control}
+            name="text"
+            maxlength="256"
+          />
+        </TextContainer>
+        <BottomContainer>
+          <BottomText>A sua mensagem será pública.</BottomText>
+          <Button disabled={!isValid}>Postar mensagem</Button>
+        </BottomContainer>
+      </form>
     </PostContainer>
   )
 }
